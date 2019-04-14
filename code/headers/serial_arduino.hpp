7da4586bcf35d851e773fef2c95f2fd20ff37b27
MartyVos <marty.vos@student.hu.nl>
2019-04-14 22:27:30 +0200

    Added code documentation

--- code/headers/serial_arduino.hpp
+++ code/headers/serial_arduino.hpp
@@ -3,20 +3,54 @@
 #include <iostream>
 #include <vector>
 
-class serial_arduino {
-private:
-    const char *port;
-    unsigned int buffer_size;
-    SerialPort *arduino;
-    std::vector<unsigned char> extract_bytes(std::string input);
+namespace r2d2::remote_control {
+    class serial_arduino_c {
+    private:
+        const char *port;
+        unsigned int buffer_size;
+        SerialPort *arduino;
 
-public:
-    serial_arduino(const char *port, unsigned int buffer_size)
-        : port(port), buffer_size(buffer_size), arduino(new SerialPort(port)) {
-    }
-    ~serial_arduino() {
-        delete arduino;
-    }
-    std::vector<unsigned char> serial_read();
-    bool isConnected();
-};
+        /**
+         * This method extracts bytes from a std::string and puts them in
+         * a std::vector<unsigned char>.
+         * @param input
+         * @return std::vector<unsigned char>
+         */
+        std::vector<unsigned char> extract_bytes(std::string input);
+
+    public:
+        /**
+         * This constructor creates a new serial_arduino_c object using a
+         * COM-port and a buffer size.
+         * @param port
+         * @param buffer_siz
+         */
+        serial_arduino_c(const char *port, unsigned int buffer_size)
+            : port(port),
+              buffer_size(buffer_size),
+              arduino(new SerialPort(port)) {
+        }
+
+        /**
+         * This destructor destroys a serial_arduino_c object.
+         */
+        ~serial_arduino_c() {
+            delete arduino;
+        }
+
+        /**
+         * This method returns the serial input data as an std::vector<unsigned
+         * char>.
+         * @return std::vector<unsigned char>
+         */
+        std::vector<unsigned char> serial_read();
+
+        /**
+         * This method checks of there is a connection between the PC and the
+         * Arduino and returns true for a connection and false for no
+         * connection.
+         * @return bool
+         */
+        bool isConnected();
+    };
+} // namespace r2d2::remote_control
