63914a055787e4404c90e03608203478ff43a57d
MarkGasse <mark.gasse@student.hu.nl>
2019-05-05 22:25:35 +0200

    Added documentation and removed *.ipch files

--- code/headers/steering_wheel_controller.hpp
+++ code/headers/steering_wheel_controller.hpp
@@ -2,6 +2,11 @@
 
 #include <hwlib.hpp>
 
+/**
+ * \brief
+ * This class is for reading the data from the steering wheel
+ */
+
 class steering_wheel_controller_c : public hwlib::pin_in {
 private:
     hwlib::target::pin_adc &wheel;
@@ -16,6 +21,14 @@ public:
                                 hwlib::target::pin_adc &pedals,
                                 hwlib::pin_in &button1, hwlib::pin_in &button2,
                                 hwlib::pin_in &button3, hwlib::pin_in &button4);
+
+    /**
+     * Refreshes the data of the read buffer and reads the data.
+     */
     bool read() override;
+
+    /**
+     * Reads and prints the recieved data from the steering wheel.
+     */
     void print();
 };
