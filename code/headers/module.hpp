376d50e798229904a057c0b106d24c07d58ccfdd
MarkGasse <mark.gasse@student.hu.nl>
2019-05-13 09:53:08 +0200

    Added/updated documentation

--- code/headers/module.hpp
+++ code/headers/module.hpp
@@ -4,20 +4,33 @@
 #include <steering_wheel_controller.hpp>
 
 namespace r2d2::manual_control {
+
+    /**
+     * Class module_c provides the process function
+     * for receiving data requests
+     * and sending the data of the steering_wheel_controller_c.
+     */
     class module_c : public base_module_c {
     private:
         steering_wheel_controller_c &steering_wheel;
 
     public:
+        /**
+         * Instantiate the module class
+         *
+         * @param comm - The internal communication bus instance.
+         * @param steering_wheel - The steering wheel controller that represents
+         * the steering wheel/manual control.
+         */
         steering_wheel_controller_c(base_comm_c &comm, steering_wheel)
             : base_module_c(comm), steering_wheel(steering_wheel) {
 
-            comm.listen_for_frames({
-                frame_type::MANUAL_CONTROL // frame_type not yet implemented in
-                                           // internal communication
-            });
+            comm.listen_for_frames({frame_type::MANUAL_CONTROL});
         }
 
+        /**
+         * This function lets the module process data
+         */
         void process() override {
             frame_manual_control_state_s<4, 2> frame;
             frame.buttons[0] = steering_wheel.get_button(0);
@@ -31,12 +44,16 @@ namespace r2d2::manual_control {
             while (comm.has_data()) {
                 auto frame = comm.get_data();
 
-                // Is this frame a request?
+                /*
+                 * Only process frame if it is a request
+                 */
                 if (!frame.request) {
                     continue;
                 }
 
-                // Send it off!
+                /*
+                 * Send it off!
+                 */
                 comm.send(frame);
             }
         }
--- code/headers/steering_wheel_controller.hpp
+++ code/headers/steering_wheel_controller.hpp
@@ -5,8 +5,8 @@
 namespace r2d2::manual_control {
 
     /**
-     * \brief
-     * This class is for reading the data from the steering wheel
+     * Class steering_wheel_controller_c provides functions
+     * for reading the data of the trust steering wheel.
      */
     class steering_wheel_controller_c : public hwlib::pin_in {
     private:
@@ -14,6 +14,16 @@ namespace r2d2::manual_control {
         std::array<hwlib::target::pin_adc *, 2> sliders;
 
     public:
+        /**
+         * Instantiate the steering wheel controller class
+         * 
+         * @param button1 The digital input that reads the button state.
+         * @param button2 The digital input that reads the button state.
+         * @param button3 The digital input that reads the button state.
+         * @param button4 The digital input that reads the button state.
+         * @param wheel The adc input that reads the data of the potmeter.
+         * @param pedals The adc input that reads the data of the potmeter.
+         */
         steering_wheel_controller_c(hwlib::target::pin_in &button1,
                                     hwlib::target::pin_in &button2,
                                     hwlib::target::pin_in &button3,
@@ -22,16 +32,34 @@ namespace r2d2::manual_control {
                                     hwlib::target::pin_adc &pedals);
 
         /**
-         * Refreshes the data of the read buffer and reads the data.
+         * This function overrides the read() function of pin_in
+         * and reads the data of a pin_in.
+         * 
+         * @internal refreshes/reads the incoming data.
          */
         bool read() override;
 
+        /**
+         * This function gets the data of a slider
+         *
+         *  @param slider - Choose the slider you want to get.
+         *  @internal Reads the data of the slider.
+         *  @return the data of the slider (type: uint8_t).
+         */
         uint8_t get_slider(const int &slider);
 
+        /**
+         * This function gets the data of a button.
+         *
+         *  @param slider - Choose the button you want to get.
+         *  @internal Reads the data of the button.
+         *  @return the data of the button (type: boolean).
+         */
         bool get_button(const int &button);
 
         /**
-         * Reads and prints the recieved data from the steering wheel.
+         * This function is for testing/reading
+         * the incoming data of the steering wheel.
          */
         void print();
     };
