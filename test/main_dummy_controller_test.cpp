3a47f6a630d4c4df1cbbd4985da314dca07fcb40
B9a9s9 <bas.vaneck@ziggo.nl>
2019-06-19 13:04:47 +0200

    Catch2 tests for dummy/interface.

--- test/main_dummy_controller_test.cpp
+++ test/main_dummy_controller_test.cpp
@@ -1,54 +1,37 @@
-#include "hwlib.hpp"
+#include "ostream"
 #include <dummy_controller_c.hpp>
 
-int main(void) {
-  // kill the watchdog
-    WDT->WDT_MR = WDT_MR_WDDIS;
-    hwlib::wait_ms(1000);
-      hwlib::cout << "this works via arduino\n";
-      hwlib::wait_ms(1000);
-      auto dummy_controller = r2d2::manual_control::dummy_controller_c();
-      hwlib::cout << "all buttons:\n";
-      for(int x = r2d2::manual_control::button_a; x <= r2d2::manual_control::menu_right; x++){
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+TEST_CASE ( "testing buttons", " [interface]" ){
+    auto dummy_controller = r2d2::manual_control::dummy_controller_c();
+    for(int x = r2d2::manual_control::button_a; x <= r2d2::manual_control::menu_right; x++){
         //check buttons
-          r2d2::manual_control::buttons button = static_cast<r2d2::manual_control::buttons>(x);
-          hwlib::cout << "\nbutton " << button << "\nbefore button changed: " << dummy_controller.get_button(button) << "\n";
+        r2d2::manual_control::buttons button = static_cast<r2d2::manual_control::buttons>(x);
+        CHECK ( dummy_controller.get_button(button) == 0 );
+        dummy_controller.set_button(button, 1);
+        CHECK ( dummy_controller.get_button(button) == 1 );
+    }
 
-        //change button state
-          dummy_controller.set_button(button, 1);
-          hwlib::cout << "after button changed: ";
+    //check sliders
+    CHECK( dummy_controller.get_slider(r2d2::manual_control::slider_l) == 0 ); 
+    CHECK( dummy_controller.get_slider(r2d2::manual_control::slider_r) == 0 );
+    dummy_controller.set_slider(r2d2::manual_control::slider_l, 1); 
+    dummy_controller.set_slider(r2d2::manual_control::slider_r, 1);
+    CHECK( dummy_controller.get_slider(r2d2::manual_control::slider_l) == 1 ); 
+    CHECK( dummy_controller.get_slider(r2d2::manual_control::slider_r) == 1 );
 
-        //check buttons
-          hwlib::cout << dummy_controller.get_button(button) << "\n";
-      }
-
-      //check sliders
-      hwlib::cout << "\n\nsliders:\nslider_l: " << dummy_controller.get_slider(r2d2::manual_control::slider_l);
-      hwlib::cout << "\nslider_r: " << dummy_controller.get_slider(r2d2::manual_control::slider_r);
-
-      //change slider state
-      dummy_controller.set_slider(r2d2::manual_control::slider_l, 50);
-      dummy_controller.set_slider(r2d2::manual_control::slider_r, 50);
-
-      //check sliders
-      hwlib::cout << "\n\nsliders after change:\nslider_l: " << dummy_controller.get_slider(r2d2::manual_control::slider_l);
-      hwlib::cout << "\nslider_r: " << dummy_controller.get_slider(r2d2::manual_control::slider_r);
-
-      //check joysticks
-      hwlib::cout << "\n\njoysticks:\njoystick_l: " << dummy_controller.get_joystick(r2d2::manual_control::joystick_l).x;
-      hwlib::cout << ", " << dummy_controller.get_joystick(r2d2::manual_control::joystick_l).y;
-      hwlib::cout << "\njoystick_r: " << dummy_controller.get_joystick(r2d2::manual_control::joystick_r).x;
-      hwlib::cout << ", " << dummy_controller.get_joystick(r2d2::manual_control::joystick_r).y;
-
-      //change joystick state
-      dummy_controller.set_joystick(r2d2::manual_control::joystick_l, {50, 46});
-      dummy_controller.set_joystick(r2d2::manual_control::joystick_r, {68, 100});
-
-      //check joysticks
-      hwlib::cout << "\n\njoysticks after change:\njoystick_l: " << dummy_controller.get_joystick(r2d2::manual_control::joystick_l).x;
-      hwlib::cout << ", " << dummy_controller.get_joystick(r2d2::manual_control::joystick_l).y;
-      hwlib::cout << "\njoystick_r: " << dummy_controller.get_joystick(r2d2::manual_control::joystick_r).x;
-      hwlib::cout << ", " << dummy_controller.get_joystick(r2d2::manual_control::joystick_r).y;
-
+    //check joysticks
+    CHECK( dummy_controller.get_joystick(r2d2::manual_control::joystick_l).x == 0 ); 
+    CHECK( dummy_controller.get_joystick(r2d2::manual_control::joystick_l).y == 0 ); 
+    CHECK( dummy_controller.get_joystick(r2d2::manual_control::joystick_r).x == 0 );
+    CHECK( dummy_controller.get_joystick(r2d2::manual_control::joystick_r).y == 0 );
+    dummy_controller.set_joystick(r2d2::manual_control::joystick_l, {1, 1} ); 
+    dummy_controller.set_joystick(r2d2::manual_control::joystick_r, {1, 1} );
+    CHECK( dummy_controller.get_joystick(r2d2::manual_control::joystick_l).x == 1 ); 
+    CHECK( dummy_controller.get_joystick(r2d2::manual_control::joystick_l).y == 1 ); 
+    CHECK( dummy_controller.get_joystick(r2d2::manual_control::joystick_r).x == 1 );
+    CHECK( dummy_controller.get_joystick(r2d2::manual_control::joystick_r).y == 1 );
 
 }
