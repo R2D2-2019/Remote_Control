9afd815fc5bae76985a962e769d2438822971441
B9a9s9 <bas.vaneck@ziggo.nl>
2019-07-04 12:04:39 +0200

    code edited so it works if multiple letters are pressed at once

--- python_code/keyboard_handler.py
+++ python_code/keyboard_handler.py
@@ -3,26 +3,53 @@
 #to use this code, first run the following command:
 #pip install -r requirements.txt
 
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener, KeyCode
 import serial
+import numpy as np
 
-ser = serial.Serial('COM2', 9600)  # open serial port
-pressedKeys = set()
+ser = serial.Serial('COM2', 2400)  # open serial port
+ser.write(0b00010000)
 
+
+controller_state = np.uint8(0)
 #if the key wasn't already pressed, output on serial
 def on_press(key):
-    if key not in pressedKeys:
-        ser.write(bytes(str(key), encoding='utf-8'))
-    pressedKeys.add(key)
+    global controller_state
+    print(key)
+    preState = controller_state
+
+    if key.char == 'a' or key.char == 'A':
+        controller_state = controller_state | 0b0000001
+
+    if key.char == 'd' or key.char == 'D':
+        controller_state = controller_state | 0b0000010
+
+    if key.char == 'w' or key.char == 'W':
+        controller_state = controller_state | 0b0000100
+
+    if key.char == 's' or key.char == 'S':
+        controller_state = controller_state | 0b0001000
+
+    if preState != controller_state:
+        ser.write(bytes(str(controller_state), encoding='utf-8'))
 
 #output key on serial
 def on_release(key):
-    ser.write(bytes(str(key), encoding='utf-8'))
-    pressedKeys.remove(key)
+    global controller_state
 
-    if key == Key.esc:
-        # Stop listener
-        return False
+    if key.char == 'a' or key.char == 'A':
+        controller_state = controller_state & 0b1111110
+
+    if key.char == 'd' or key.char == 'D':
+        controller_state = controller_state & 0b11111101
+
+    if key.char == 'w' or key.char == 'W':
+        controller_state = controller_state & 0b11111011
+
+    if key.char == 's' or key.char == 'S':
+        controller_state = controller_state & 0b11110111
+
+    ser.write(bytes(str(controller_state), encoding='utf-8'))
 
 # Collect events until released
 with Listener(
---
+++ python_code/readme.md
@@ -0,0 +1,3 @@
+the file keyboard_handler.py contains code that reads keyboard input and sends a byte serial to the ardruino over comm2.
+This code is used for demo's. To actually use this code, the code needs to be perfected. 
+ 
