c230595e2daecfcea62292139edd491b40b8b00c
Lars Versteeg <lars.versteeg1999@gmail.com>
2019-08-22 13:55:09 +0200

    Added check to see if data in invalid

--- code/src/ps2_mat.cpp
+++ code/src/ps2_mat.cpp
@@ -6,5 +6,8 @@ ps2_mat_c::ps2_mat_c(ps2_bus_c &ps2_bus):
 
 std::array<uint8_t, 8> ps2_mat_c::read_mat(){
     std::array<uint8_t, 8> data = ps2_bus.read_write(poll_command, 8);
+    if(!(data[1] == 65 && data[2] == 90)){
+        data.fill(255);
+    }
     return data;
 }
